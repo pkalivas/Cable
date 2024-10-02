4263cb8004201274d9a50b1a5b75ae288a47ee30
Peter Kalivas <peterkalivas@gmail.com>
2024-10-02 18:50:17 -0500

    Well

--- Cable.Examples/Program.cs
+++ Cable.Examples/Program.cs
@@ -13,27 +13,44 @@ public class TestHandler : IMessageHandler<MOne>,
     IMessageHandler<MTwo>, 
     IMessageHandler<MThree>
 {
+    public int Count { get; set; } = 0;
+    
+    public TestHandler()
+    {
+        Console.WriteLine("TestHandler created.");
+    }
+    
     public async Task Handle(MOne message)
     {
-        Console.WriteLine($"Received Message {message.Data} MOne");
+        Count++;
+        Console.WriteLine($"Received Message {message.Data} MOne in Test Handler. Count: {Count}");
     }
 
     public async Task Handle(MTwo message)
     {
-        Console.WriteLine($"Received Message {message.Data} from MTwo");
+        Count++;
+        Console.WriteLine($"Received Message {message.Data} from MTwo in Test Handler. Count: {Count}");
     }
 
     public async Task Handle(MThree message)
     {
-        Console.WriteLine($"Received Message {message.Data} from MThree");
+        Count++;
+        Console.WriteLine($"Received Message {message.Data} from MThree in Test Handler. Count: {Count}");
     }
 }
 
 public class TestTwoHandler : IMessageHandler<MTwo>
 {
+    public int Count { get; set; } = 0;
+    
+    public TestTwoHandler()
+    {
+        Console.WriteLine("TestTwoHandler created.");
+    }
     public async Task Handle(MTwo message)
     {
-        Console.WriteLine($"Received Message {message.Data} from MTwo in TestTwo");
+        Count++;
+        Console.WriteLine($"Received Message {message.Data} from MTwo in TestTwo Handler. Count: {Count}");
     }
 }
 
@@ -44,6 +61,8 @@ public class Program
         var bus = new Transport(AppDomain.CurrentDomain);
         
         await bus.Publish(new MTwo("HEY LOOK IT WORKS."));
+        await bus.Publish(new MOne("HEY LOOK IT WORKS."));
+        await bus.Publish(new MThree("HEY LOOK IT WORKS."));
         
         var t = "";
     }
--- Cable/Core/Transport.cs
+++ Cable/Core/Transport.cs
@@ -5,50 +5,55 @@ namespace Cable.Core;
 
 public class Transport
 {
-    private readonly Dictionary<string, List<Type>> _messageHandlers;
+    private readonly Dictionary<Type, List<object>> _messageHandlerInstances = new();
 
     public Transport(AppDomain currentDomain)
     {
-        _messageHandlers = currentDomain.GetAssemblies()
-            .SelectMany(assembly => GetAllImplementingTypes(typeof(IMessageHandler<>), assembly))
-            .DistinctBy(member => member.AssemblyQualifiedName)
-            .SelectMany(handler => handler.GetInterfaces()
+        var messageHandlers = currentDomain.GetAssemblies()
+            .SelectMany(assembly => assembly.GetTypes())
+            .Where(type => type.GetInterfaces()
+                .Any(inter => inter.IsGenericType && typeof(IMessageHandler<>).IsAssignableFrom(inter.GetGenericTypeDefinition())))
+            .ToList();
+        
+        var handlerInstances = messageHandlers
+            .Select(Activator.CreateInstance)
+            .ToList();
+
+        foreach (var handler in handlerInstances)
+        {
+            var handlerType = handler.GetType();
+            var messageTypes = handlerType.GetInterfaces()
                 .Where(hand => hand.IsGenericType && hand.GetGenericTypeDefinition() == typeof(IMessageHandler<>))
                 .SelectMany(hand => hand.GetGenericArguments()
                     .Select(arg => new
                     {
                         Handler = handler,
-                        MessageType = arg.Name,
-                    })))
-            .GroupBy(val => val.MessageType)
-            .ToDictionary(key => key.Key, value => value.Select(val => val.Handler).ToList());
+                        MessageType = arg,
+                    }))
+                .GroupBy(val => val.MessageType)
+                .ToList();
+            
+            foreach (var messageType in messageTypes)
+            {
+                if (!_messageHandlerInstances.ContainsKey(messageType.Key))
+                {
+                    _messageHandlerInstances[messageType.Key] = new List<object>();
+                }
+                
+                _messageHandlerInstances[messageType.Key].Add(handler);
+            }
+        }
     }
     
     public async Task Publish<T>(T message) where T : IMessage
     {
         var messageType = message.GetType();
-
-        foreach (var handlerType in _messageHandlers[messageType.Name])
+        if (_messageHandlerInstances.TryGetValue(messageType, out var instance))
         {
-            var handler = Activator.CreateInstance(handlerType);
-            var method = handlerType.GetMethod("Handle", new[] { messageType });
-            await (Task) method.Invoke(handler, new object[] { message });   
+            foreach (var casted in instance.Cast<IMessageHandler<T>?>())
+            {
+                await casted?.Handle(message)!;
+            }
         }
     }
-
-    private static IEnumerable<Type> GetAllImplementingTypes(Type openGenericType, Assembly assembly) =>
-        assembly.GetTypes()
-            .Where(type => type.GetInterfaces()
-                .Any(inter => inter.IsGenericType && openGenericType.IsAssignableFrom(inter.GetGenericTypeDefinition())))
-            .ToList();
 }
-
-
-// _messageTypes = _handlerTypes.Values
-//     .SelectMany(handler => handler.GetInterfaces()
-//         .Where(hand => hand.IsGenericType && hand.GetGenericTypeDefinition() == typeof(IMessageHandler<>))
-//         .Select(hand => hand.GetGenericArguments())
-//         .Where(hand => hand.Any())
-//         .Select(hand => hand.First()))
-//     .DistinctBy(handler => handler.AssemblyQualifiedName)
-//     .ToDictionary(key => key.Name);
